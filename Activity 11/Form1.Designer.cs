659270b4dd4c349d32ea7a91a7bb82f1a4f6d9a5
DavidP-GCU <114124010+DavidP-GCU@users.noreply.github.com>
2022-10-22 22:15:07 -0600

    Add files via upload

---
+++ Activity 11/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Activity_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRollDice = new System.Windows.Forms.Button();
+            this.lblDice1 = new System.Windows.Forms.Label();
+            this.lblDice2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRollDice
+            // 
+            this.btnRollDice.BackColor = System.Drawing.Color.Silver;
+            this.btnRollDice.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnRollDice.FlatAppearance.BorderSize = 20;
+            this.btnRollDice.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRollDice.ForeColor = System.Drawing.Color.Black;
+            this.btnRollDice.Location = new System.Drawing.Point(29, 36);
+            this.btnRollDice.Name = "btnRollDice";
+            this.btnRollDice.Size = new System.Drawing.Size(140, 111);
+            this.btnRollDice.TabIndex = 0;
+            this.btnRollDice.Text = "Roll Dice";
+            this.btnRollDice.UseVisualStyleBackColor = false;
+            this.btnRollDice.Click += new System.EventHandler(this.btnRollDice_Click);
+            // 
+            // lblDice1
+            // 
+            this.lblDice1.BackColor = System.Drawing.Color.Silver;
+            this.lblDice1.Font = new System.Drawing.Font("Lucida Console", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDice1.ForeColor = System.Drawing.Color.Black;
+            this.lblDice1.Location = new System.Drawing.Point(223, 55);
+            this.lblDice1.Name = "lblDice1";
+            this.lblDice1.Size = new System.Drawing.Size(88, 70);
+            this.lblDice1.TabIndex = 1;
+            this.lblDice1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDice2
+            // 
+            this.lblDice2.BackColor = System.Drawing.Color.Silver;
+            this.lblDice2.Font = new System.Drawing.Font("Lucida Console", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDice2.ForeColor = System.Drawing.Color.Black;
+            this.lblDice2.Location = new System.Drawing.Point(368, 55);
+            this.lblDice2.Name = "lblDice2";
+            this.lblDice2.Size = new System.Drawing.Size(88, 70);
+            this.lblDice2.TabIndex = 2;
+            this.lblDice2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SaddleBrown;
+            this.ClientSize = new System.Drawing.Size(531, 191);
+            this.Controls.Add(this.lblDice2);
+            this.Controls.Add(this.lblDice1);
+            this.Controls.Add(this.btnRollDice);
+            this.ForeColor = System.Drawing.Color.Yellow;
+            this.Name = "Form1";
+            this.Text = "Activity 11 Dice Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRollDice;
+        private System.Windows.Forms.Label lblDice1;
+        private System.Windows.Forms.Label lblDice2;
+    }
+}
+
---
+++ Activity 11/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Object of Dice class with same number of sides
+        Dice dice1 = new Dice(5);
+        Dice dice2 = new Dice(5);
+
+        // Create int variable that stores the number of rolls
+        int numberOfRolls = 0;
+
+        private void btnRollDice_Click(object sender, EventArgs e) // Roll Dice button event handler
+        {
+            // Call Method rollDie()
+            int roll1 = dice1.rollDie();
+            int roll2 = dice2.rollDie();
+
+            // Show dice face on the labels
+            lblDice1.Text = roll1.ToString();
+            lblDice2.Text = roll2.ToString();
+
+            //Increment the value of numberOfRolls by 1 using logical operator(++)
+            numberOfRolls++;
+
+            // Create loop that checks for snake eyes
+            if (roll1 == 1 && roll2 == 1)
+            {
+                btnRollDice.Enabled = false;
+
+                // Show MessageBox
+                MessageBox.Show("It took " + numberOfRolls + " rolls to get snake eyes!");
+            }
+        }
+    }
+
+    // Create Dice Class
+        class Dice // name of class and the beginning of the class. 
+        {
+        // A private field for the number of sides of the die
+        private int numberOfSides;
+        
+        // Constructor that takes an integer between 4 and 20, inclusive and sets the number of sides of the die
+        public Dice(int numbeSides)
+        {
+            // Checks the number of sides
+            if (numbeSides >= 4 && numbeSides <= 20)
+            {
+                // When numbeSides between 4 to 20 inclusive then. 
+                numberOfSides = numbeSides;
+            }
+            else
+            {
+                // When invalid sides are entered then
+                MessageBox.Show("Number of sides should be between 4 to 20 inclusive");
+                
+            }
+        }// This is the end of the constructor
+
+        // Create Method (rollDie()) that returns the face value when the die is rolled
+        public int rollDie()
+        {
+            // Object of Random class
+            Random randomDie = new Random();
+
+            // Random number to roll the die
+            return randomDie.Next(numberOfSides);
+        }// The end of the method (rollDie())
+
+        }// The end of the class (Dice). 
+
+
+
+    
+}
